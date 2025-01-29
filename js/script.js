fb67e88223cb64e5e8d8455777fe02d106d459c9
AJOY SAHA <ajoy.code.dreamer@gmail.com>
2025-01-29 18:05:44 +0600

    one condition done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
-var age = 18;
+// person details
+var ageMinimum = 18;
+var ageLimit = 40;
 var job = true;
 var governmentJob = true;
-var salary = 50000;
+var salary = 40000;
+var married = false;
+var availableHouse = true;
+// person details
 
+// marriage condition
 
-if ( age >= 18 ) {
-    console.log('You are eligible for loan');
+if (ageMinimum >= 18 && ageLimit <= 40) {
+    if (job == true && governmentJob == true) {
+        if (salary >= 40000) {
+            if (married == false) {
+                if (availableHouse == true) {
+                    console.log("You are eligible for marriage");
+                } else {
+                    console.log("You are not eligible for marriage because you don't have house");
+                }
+            } else {
+                console.log("You are not eligible for marriage because you are already married");
+            }
+        } else {
+            console.log("You are not eligible for marriage because your salary is less than 40000");
+        }
+    } else {
+        console.log("You are not eligible for marriage because you don't have government job");
+    }
+}else{
+    console.log("You are not eligible for marriage because your age is not in between 18 to 40");
 }
-
-
